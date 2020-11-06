2dffc10f2082d28c6140c1f640b752227313ffb8
mrdouglas <mrdouglas@aiorion.ai.stonybrook.edu>
2020-11-05 19:17:12 -0500

    remove try and log

--- examples/linkproppred/biokg/try-conne2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=1 python run.py --do_train --cuda --do_valid --do_test --evaluate_train \
-  --model ConnE2 -n 128 -b 512 -d 2000 -g 20 -a 1.0 -adv \
-  -lr 0.0001 --max_steps 300000 --cpu_num 2 --test_batch_size 32
-
--- examples/linkproppred/biokg/try-transe2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-CUDA_VISIBLE_DEVICES=0 python run.py --do_train --cuda --do_valid --do_test --evaluate_train \
-  --model TransE2 -n 128 -b 512 -d 2000 -g 20 -a 1.0 -adv \
-  -lr 0.0001 --max_steps 300000 --cpu_num 2 --test_batch_size 32
-
