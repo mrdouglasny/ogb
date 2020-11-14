34d2b68200b6b93daae3add8b3eb92af925acab3
Michael R. Douglas <mdouglas@scgp.stonybrook.edu>
2020-11-14 13:19:58 -0500

    Update evaluate.py

--- ogb/linkproppred/evaluate.py
+++ ogb/linkproppred/evaluate.py
@@ -244,6 +244,7 @@ class Evaluator:
             if self.eval_metric == 'mrr2':
                 toparg = argsort[:,0]
                 nextarg = (argsort == 1).nonzero()
+                nextarg = nextarg[:, 1]
                 y_neg_mean = torch.mean(y_pred_neg,1)
                 y_neg_sd = torch.std(y_pred_neg,1)
                 print( 'score', 'rank', 'topscore', 'toparg', 'mean', 'sd', 'nextscore', 'nextarg' )
