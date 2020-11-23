9654c2e8ad15cf158f5a5e1c7488fdc6544533f8
Michael R. Douglas <mdouglas@scgp.stonybrook.edu>
2020-11-23 17:33:52 -0500

    Update run.py

--- examples/linkproppred/wikikg/run.py
+++ examples/linkproppred/wikikg/run.py
@@ -100,10 +100,10 @@ def override_config(args):
     args.dataset = argparse_dict['dataset']
     if not args.test_change_model:
         args.model = argparse_dict['model'] 
+        args.test_batch_size = argparse_dict['test_batch_size']
     args.double_entity_embedding = argparse_dict['double_entity_embedding']
     args.double_relation_embedding = argparse_dict['double_relation_embedding']
     args.hidden_dim = argparse_dict['hidden_dim']
-    args.test_batch_size = argparse_dict['test_batch_size']
     
 def save_model(model, optimizer, save_variable_list, args):
     '''
