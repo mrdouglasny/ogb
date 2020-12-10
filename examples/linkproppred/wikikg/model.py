3c9e6b760ef3d3f4b658dbbb8234f0e817c70c1d
Michael R. Douglas <mdouglas@scgp.stonybrook.edu>
2020-12-10 15:03:59 -0500

    Update model.py

--- examples/linkproppred/wikikg/model.py
+++ examples/linkproppred/wikikg/model.py
@@ -294,6 +294,7 @@ class KGEModel(nn.Module):
         return score
 
     def HeadRE(self, head, relation, tail, mode):
+        hidden_dim = head.size(1)
         re_head, scale_tail = torch.split(relation, [hidden_dim,1], dim=2)
         head = F.normalize(head, 2, -1)
         tail = F.normalize(tail, 2, -1)
@@ -302,6 +303,7 @@ class KGEModel(nn.Module):
         return score
 
     def TailRE(self, head, relation, tail, mode):
+        hidden_dim = tail.size(1)
         re_tail, scale_head = torch.split(relation, [hidden_dim,1], dim=2)
         head = F.normalize(head, 2, -1)
         tail = F.normalize(tail, 2, -1)
