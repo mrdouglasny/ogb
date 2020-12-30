f3cc8bfd57af313d097dcaec5aa0840ac59c71e9
Michael R. Douglas <mdouglas@scgp.stonybrook.edu>
2020-12-30 13:03:34 -0500

    Update readnp.py

--- examples/linkproppred/random/readnp.py
+++ examples/linkproppred/random/readnp.py
@@ -14,9 +14,16 @@ def parse_args(args=None):
 
     parser.add_argument('infile', type=str)
     parser.add_argument('outfile', type=str)
+    parser.add_argument('-pn', '--print_norms', action='store_true')
     return parser.parse_args(args)
 
 args = parse_args()
 
 data = np.load(args.infile)
-np.savetxt(args.outfile, data, fmt='%.8e', header='Read from ' + args.infile)
+
+if args.print_norms:
+    print( 'norms:', np.linalg.norm(data, ord=1, axis=0) )
+    motif = data[0,]+data[1,]-data[2,]
+    print( 'motif:', np.linalg.norm(motif, ord=1) )    
+else:
+    np.savetxt(args.outfile, data, fmt='%.8e', header='Read from ' + args.infile)
