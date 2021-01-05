0d02f6c3336cbd663043cbae6b1b8eac9376c11b
Michael R. Douglas <mdouglas@scgp.stonybrook.edu>
2021-01-05 12:55:49 -0500

    Update model.py

--- examples/linkproppred/wikikg/model.py
+++ examples/linkproppred/wikikg/model.py
@@ -607,7 +607,7 @@ class KGEModel(nn.Module):
                         logging.info('Evaluating the model... (%d/%d)' %
                                      (step, total_steps))
                         if step>0 and args.test_dump_byrel:
-                            print('step neg relation hist', file=dump)
+                            print('step neg relation mean sd hist', file=dump)
                             for i in range(2):
                                 for j in range(args.nrelation):
                                     m = hist_byrel[i][j].mean()
